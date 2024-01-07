93f55d4709b2cf7531c30d67436bee488db7bda6
Nickchen Nick <nickchen120235@gmail.com>
2024-01-07 11:48:24 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import type { HttpFunction } from '@google-cloud/functions-framework/build/src/functions'
+import { PubSub } from '@google-cloud/pubsub'
+
+const pubsub = new PubSub()
+
+export const main: HttpFunction = async (req, res) => {
+  const data = {
+    method: req.method,
+    path: req.path
+  }
+  try {
+    const messageId = await pubsub.topic('test-topic').publishMessage({ json: data })
+    res.status(200).json({ message: `Message ${messageId} published.` })
+  }
+  catch (err) {
+    res.status(500).json({ err })
+  }
+}
