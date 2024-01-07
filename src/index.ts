d9bf5988b8a00c1e9132f5a472fe42ad316be500
Nickchen Nick <nickchen120235@gmail.com>
2024-01-07 17:10:38 +0800

    use attributes to pass json

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ export const main: HttpFunction = async (req, res) => {
     path: req.path
   }
   try {
-    const messageId = await pubsub.topic('test-topic').publishMessage({ json: data })
+    const messageId = await pubsub.topic('test-topic').publishMessage({ attributes: data })
     res.status(200).json({ message: `Message ${messageId} published.` })
   }
   catch (err) {
